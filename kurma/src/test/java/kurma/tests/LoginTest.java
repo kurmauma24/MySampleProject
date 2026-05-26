351be22f8506adc139baa8ea22ec8e9b5e105935
uma <kurmauma24@gmail.com>
2026-05-27 01:36:19 +0530

    Added second update

--- kurma/src/test/java/kurma/tests/LoginTest.java
+++ kurma/src/test/java/kurma/tests/LoginTest.java
@@ -1,8 +1,17 @@
 package kurma.tests;
 
+import org.testng.annotations.BeforeClass;
+
 public class LoginTest {
 
 	public class DemoTest {
 
+
+		@BeforeClass
+
+	    public void start() {
+
+	       
+	    }
 	}
 }
