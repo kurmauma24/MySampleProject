1bc6efa5bf91c18cc114e18ff22e893da17e8734
kurmauma24 <kurmauma24@gmail.com>
2026-06-12 12:54:10 +0530

    Delete src directory

--- src/main/java/utilities/ReportManager.java
+++
@@ -1,9 +0,0 @@
-package utilities;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class ReportManager {
-
-	public static String runFolder = "Run_" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-}
